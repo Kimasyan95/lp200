aa11f902ce0b3df61281371ba28be0be74b68ad9
Kimasyan95 <71349030+Kimasyan95@users.noreply.github.com>
2021-10-19 15:40:24 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
     $(this).parent('.video--small').append(`<iframe src="https://www.youtube.com/embed/${$(this).data('id')}?autoplay=1&autohide=1&version=3&hl=ru_RU&rel=0" frameborder="0" style="width: 100%; height: 240px;"></iframe>`)
 
+    $(this).parent('.video--big').append(`<iframe src="https://www.youtube.com/embed/${$(this).data('id')}?autoplay=1&autohide=1&version=3&hl=ru_RU&rel=0" frameborder="0" style="width: 100%; height: 530px;"></iframe>`)
+
     $(this).remove();
   });
 
